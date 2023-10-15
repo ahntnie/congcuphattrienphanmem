b9efbf958472f57d6c73c7d371e46005413c0765
Admin <Admin@ADMIN-PC>
2023-10-15 13:52:08 +0700

    personal

--- lib/components/personal.dart
+++ lib/components/personal.dart
@@ -5,6 +5,41 @@ class PersonalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              "THÔNG TIN CÁ NHÂN",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.red, fontWeight: FontWeight.bold, fontSize: 50),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            Padding(padding: EdgeInsets.all(16.0)),
+            Text("Họ tên: Nguyễn Tấn Phước")
+          ],
+        ),
+        Row(children: [
+          Padding(padding: EdgeInsets.all(16.0)),
+          Text("Ngày sinh: 22/10/2003")
+        ]),
+        Row(children: [
+          Padding(padding: EdgeInsets.all(16.0)),
+          Text("Giới tính: Nam")
+        ]),
+        Row(children: [
+          Padding(padding: EdgeInsets.all(16.0)),
+          Text("Số điện thoại: 0969475617")
+        ]),
+        Row(children: [
+          Padding(padding: EdgeInsets.all(16.0)),
+          Text("Địa chỉ: 0969475617")
+        ])
+      ],
+    );
   }
 }

9831cc454eb664a4975cc61b389a34dac118a65e
Tấn Phước Nguyễn <ngguyentanphuoc2003@gmail.com>
2023-10-15 11:54:07 +0700

    HomeScreen

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -5,6 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      mainAxisAlignment:MainAxisAlignment.center,
+      children: [
+        
+      ],
+    );
   }
 }

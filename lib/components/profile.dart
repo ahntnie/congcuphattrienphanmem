1b12a16dd7c87bfde963c551fbfcb4badbe6a9f2
ahntnie <bachanhtiendp3004@gmail.com>
2023-10-17 12:51:50 +0700

    anhtien profile

--- lib/components/profile.dart
+++ lib/components/profile.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
-  @override
+  @override 
   Widget build(BuildContext context) {
     return const Placeholder();
   }

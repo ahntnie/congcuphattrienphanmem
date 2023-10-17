456c4a4b7288a1f5a53de980404bf3258a7436b3
dinhanhnguyenn <dinhanhnguyen2k3@gmail.com>
2023-10-17 13:03:00 +0700

    aloalo

---
+++ lib/components/bug.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class bug extends StatelessWidget {
+  const bug({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
